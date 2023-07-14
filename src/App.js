00b75f6dc800424a3bd95466a60e5a84cf338756
Hilll19 <hilworking0110@gmail.com>
2023-07-14 23:22:12 +0700

    import home to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Home from "./Pages/Home";
+import { Login } from "./Pages/Login";
 import { Register } from "./Pages/Register";
 import "./style.scss"
 
 function App() {
   return (
-    <Register/>
+    <Home/>
   );
 }
 export default App;
