3b0ae66b11d2aaf6b35f272d2aabad7f19898b8b
Hilll19 <hilworking0110@gmail.com>
2023-07-15 21:51:37 +0700

    Add image, Messages, Input to chat section

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Cam from "../image/cam.png";
+import AddFriend from "../image/add-friend.png";
+import SetMore from "../image/more.png";
+import Messages from "./Messages";
+import Input from "./Input"
 
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
-  )
-}
+    <div className="chat">
+      <div className="chatInfo">
+        <span>Brian</span>
+        <div className="chatIcons">
+          <img src={Cam} alt="" />
+          <img src={AddFriend} alt="" />
+          <img src={SetMore} alt="" />
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
+  );
+};
 
-export default Chat
+export default Chat;
