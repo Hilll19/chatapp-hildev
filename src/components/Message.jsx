bb83d2dabed55db034ec6043e702cf1fed3dae09
Hilll19 <hilworking0110@gmail.com>
2023-07-15 22:45:54 +0700

    Add image and span

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Ayano from '../image/Ayanokoji.png'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message'>
+      <div className="messageInfo">
+        <img src={Ayano} alt="" />
+        <span>Just Now</span>
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src={Ayano} alt="" />
+      </div>
+    </div>
   )
 }
 
