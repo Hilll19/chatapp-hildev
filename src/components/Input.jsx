5013f9f4362b46fb5afd99494ab349d554b1d197
Hilll19 <hilworking0110@gmail.com>
2023-07-15 22:21:49 +0700

    To input type something, file and img attach

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Attach from '../image/attach.png'
+import imgInput from '../image/imgInput.png'
 
 const Input = () => {
   return (
-    <div>Input</div>
+    <div className='inputContainer'>
+      <input type="text" placeholder='Type something...' />
+      <div className="send">
+        <img src={Attach} alt="" />
+        <input type="file" style={{display:"none"}} id='file' />
+        <label htmlFor="file">
+          <img src={imgInput} alt="" />
+        </label>
+        <button>Send</button>
+      </div>
+    </div>
   )
 }
 
