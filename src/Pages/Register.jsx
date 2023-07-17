9e083471122e3f467e8eefb69bdae58ab00871f5
Hilll19 <hilworking0110@gmail.com>
2023-07-17 22:33:10 +0700

    update code and check console log res

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,23 +1,42 @@
 import React, { useState } from "react";
 import Add from "../image/label.png";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage } from "../firebase";
+import {  ref, uploadBytesResumable, getDownloadURL,} from "firebase/storage";
 
 export const Register = () => {
-  const [err, setErr] = useState(false)
+  const [err, setErr] = useState(false);
   const handleSubmit = async (e) => {
     // So that when we click button, page wont reload
     e.preventDefault();
     // for checking what we want to input value can check console.log(e.target[0].value)
-    const displayName = (e.target[0].value);
-    const email = (e.target[1].value);
-    const password = (e.target[2].value);
-    const file = (e.target[3].files[0]);
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
 
-    try{
-      const res = await createUserWithEmailAndPassword(auth, email, password)
-    }catch(err){
-      setErr(true)
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(res);
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+        },
+        () => {
+          // Handle successful uploads on complete
+          // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+          getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
+            await updateProfile(res.user,{})
+            displayName,
+            photoURL: downloadURL,
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
     }
   };
 
