f797c61f6b59b36f6f6b4014a27bf90d89cc42c2
Hilll19 <hilworking0110@gmail.com>
2023-07-20 23:18:14 +0700

    Use react router dom browserrouter, routes, route

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import Home from "./Pages/Home";
 import { Login } from "./Pages/Login";
 import { Register } from "./Pages/Register";
 import "./style.scss"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Register/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="register" element={<Register/>}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 export default App;
